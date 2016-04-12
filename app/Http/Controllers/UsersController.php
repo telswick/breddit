92b517810c1119170df11a2c8d2acefe4fe19ba7
Traci Elswick <telswick@austin.rr.com>
2016-04-11 22:20:45 -0500

    Add gulp build command to composer.json

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,8 +1,12 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
+
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+
 class UsersController extends Controller
 {
     /**
